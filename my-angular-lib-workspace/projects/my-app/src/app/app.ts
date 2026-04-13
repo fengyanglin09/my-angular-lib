88e86c8a50e94dd39ce1f8006d980c4f7e9f79fa
lin.pengpeng <lin.pengpeng@mayo.edu>
2026-04-12 19:27:20 -0500

    feat: update spaa-angular-lib to version 0.0.3 and adjust imports in app component

--- my-angular-lib-workspace/projects/my-app/src/app/app.ts
+++ my-angular-lib-workspace/projects/my-app/src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Button } from '@eagle009/my-lib';
+import { Button } from '@eagle009/spaa-angular-lib/src/lib/button/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Button],
+  imports: [RouterOutlet, Button, Button],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('my-app');
